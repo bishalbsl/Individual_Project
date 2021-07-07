b2ad9060174e10d768ce0664a7072bb6b5426f49
bishal <bsalsakya@gmail.com>
2021-07-07 20:43:21 +0900

    commit ccc

--- JS/customJs.js
+++ JS/customJs.js
@@ -1,59 +1,51 @@
-	$(document).ready(function(){
-		$('a').click(function(){
-			//alert("click")
-		var selected=$(this);
-		$('a').removeClass('active');
-		$(selected).addClass('active');
-		});
+(function($) {
+    "use strict";
 
-		var $a=$('.a'),
-		$b=$('.b'),
-		$c=$('.c'),
-		$d=$('.d'),
-		$e=$('.e'),
-		$home=$('.home'),
-		$aboutme=$('.aboutme');
-		$gallery=$('.gallery');
-		$career=$('.career');
-		$contactme=$('.contactme');
+    // Navbar on scrolling
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+    });
 
-		$a.click(function(){
-			$home.fadeIn();
-			$aboutme.fadeOut();
-			$gallery.fadeOut();
-			$career.fadeOut();
-			$contactme.fadeOut();
-		});
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
 
-		$b.click(function(){
-			$aboutme.fadeIn();
-			$home.fadeOut();
-			$gallery.fadeOut();
-			$career.fadeOut();
-			$contactme.fadeOut();
-		});
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
 
-		$c.click(function(){
-			$gallery.fadeIn();
-			$home.fadeOut();
-			$aboutme.fadeOut();
-			$career.fadeOut();
-			$contactme.fadeOut();
-		});
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
 
-		$d.click(function(){
-			$career.fadeIn();
-			$contactme.fadeOut();
-			$home.fadeOut();
-			$aboutme.fadeOut();
-			$gallery.fadeOut(); 
-		});
+    // Skills
+    $('.skill').waypoint(function() {
+        $('.progress .progress-bar').each(function() {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, { offset: '80%' });
 
-		$e.click(function(){
-			$career.fadeOut();
-			$contactme.fadeIn();
-			$home.fadeOut();
-			$aboutme.fadeOut();
-			$gallery.fadeOut(); 
-		});
-	});
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function() {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    });
+
+
+
+})(jQuery);
